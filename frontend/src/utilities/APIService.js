8645cdb0c5cb016059e079c604eb33c6bc751f70
Inae Cho <inae.cho.mil@army.mil>
2026-05-09 18:58:28 -0500

    Moved UserAPIService.js and PostAPIService.js code into 1 file alled APIService.js.

---
+++ frontend/src/utilities/APIService.js
@@ -0,0 +1,82 @@
+import axios from 'axios';
+
+// User API Service
+export const axiosSavePost = async (post) => {
+    try {
+        const response = await axios.post('/api/v1/post', post);
+        return response.data;
+    } catch (error) {
+        console.error('Error saving post:', error);
+        throw error;
+    }
+};
+
+export const axiosFindAllPostsByCheckpointId = async (id) => {
+    try {
+        const response = await axios.get(`/api/v1/posts/${id}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+        throw error;
+    }
+};
+
+export const axiosUpdatePost = async (post) => {
+    try {
+        const response = await axios.put ('/api/v1/post', post);
+        return response.data;
+    } catch (error) {
+        console.error('Error updating post:', error);
+        throw error;
+    }
+};
+
+export const axiosDeleteExistingPost = async (id) => {
+    try {
+        await axios.delete(`/api/v1/post/${id}`);
+    } catch (error) {
+        console.error(`Error deleting post with id ${id}:`, error);
+        throw error;
+    }
+};
+
+
+// Post API Service
+export const axiosSavePost = async (post) => {
+    try {
+        const response = await axios.post('/api/v1/post', post);
+        return response.data;
+    } catch (error) {
+        console.error('Error saving post:', error);
+        throw error;
+    }
+};
+
+export const axiosFindAllPostsByCheckpointId = async (id) => {
+    try {
+        const response = await axios.get(`/api/v1/posts/${id}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+        throw error;
+    }
+};
+
+export const axiosUpdatePost = async (post) => {
+    try {
+        const response = await axios.put ('/api/v1/post', post);
+        return response.data;
+    } catch (error) {
+        console.error('Error updating post:', error);
+        throw error;
+    }
+};
+
+export const axiosDeleteExistingPost = async (id) => {
+    try {
+        await axios.delete(`/api/v1/post/${id}`);
+    } catch (error) {
+        console.error(`Error deleting post with id ${id}:`, error);
+        throw error;
+    }
+};
